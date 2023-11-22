7ef0da06153be3e240dca522b769a2d4c18c27f4
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-21 23:28:03 -0600

    Error handler app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """API"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def remove(exc):
     storage.close()
+
+
+app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error:": "Not found"}), 404
 
 
 if __name__ == '__main__':
