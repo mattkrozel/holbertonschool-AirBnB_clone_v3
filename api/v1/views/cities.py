2e155e9be4cc83aa7c3fcf41f8c45566c0b08210
Matt Krozel <mattkrozel@gmail.com>
2023-11-22 12:13:31 -0800

    fixed module import for all files

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ cities blueprint
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
+from models.state import State
 from models.city import City
 
 
