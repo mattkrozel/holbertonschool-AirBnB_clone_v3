e1c43c8b25d601e7ee4cfcf6e827642f5b9ca3e3
Matt Krozel <mattkrozel@gmail.com>
2023-11-22 12:46:34 -0800

    edited all files for doc module

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,6 +8,7 @@ from flask import jsonify, abort, request
 from models.user import User
 from models import storage
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id=None):
