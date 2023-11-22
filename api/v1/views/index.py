955477c93c34b03dbb912c175ca074699df04593
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-21 21:23:58 -0600

    created endpoints

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """Returns JSON status"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
 @app_views.route('/status')
 def json_status():
     return jsonify(status="OK")
+
+
+@app_views.route('/status', methods=['GET'])
+def get_stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
