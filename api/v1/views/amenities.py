06130c82039c59806b87692e201981c5e30b6010
Matt Krozel <mattkrozel@gmail.com>
2023-11-27 06:52:56 -0800

    fixed amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -9,25 +9,24 @@ from models import storage
 from models.amenity import Amenity
 
 
-
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+@app_views.route('/amenities/<a_id>', methods=['GET'],
                  strict_slashes=False)
-def get_amenity(amenity_id=None):
+def get_amenity(a_id=None):
     '''
     gets list of amenities
     '''
-    if amenity_id is None:
+    if a_id is None:
         amenity = storage.all('Amenity')
         amenities = [value.to_dict() for key, value in amenity.items()]
         return jsonify(amenities)
-    amenities = storage.get('Amenity', amenity_id)
+    amenities = storage.get('Amenity', a_id)
     if amenities is None:
         abort(404)
     return jsonify(amenities.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+@app_views.route('/amenities/<a_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(a_id):
     '''
@@ -52,25 +51,26 @@ def make_amenity():
     if name is None:
         return (jsonify({'error': 'Missing name'}), 400)
     new_amenity = Amenity()
-    new_amenity.name()
+    new_amenity.name = name
+    new_amenity.save()
     return (jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+@app_views.route('/amenities/<a_id>', methods=['PUT'],
                  strict_slashes=False)
-def update_amenity(amenity_id):
+def update_amenity(a_id):
     '''
     updates specific amenity
     '''
     data = request.get_json()
     if data is None:
         return (jsonify({'error': 'Not a JSON'}), 400)
-    amenity = storage.get('Amenity', amenity_id)
+    amenity = storage.get('Amenity', a_id)
     if amenity is None:
         abort(404)
     disallowed = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
-        if key in disallowed:
+        if key not in disallowed:
             setattr(amenity, key, value)
     amenity.save()
     return jsonify(amenity.to_dict(), 200)
